caf067a0d2a71bbc95010c7bf46709af169abb93
charisse <charisse5b@gmail.com>
2015-07-08 00:48:04 +0800

    ssd
    dsds

---
+++ Payroll/Payroll.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Payroll.Web.Startup))]
+namespace Payroll.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
